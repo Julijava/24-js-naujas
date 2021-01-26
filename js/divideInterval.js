7222391ff7bc4031773b701fda61db6b2e9f249c
Julija <varnaite.j@gmail.com>
2021-01-26 17:36:59 +0200

    Veiksmai commit

--- js/divideInterval.js
+++ js/divideInterval.js
@@ -1,12 +1,12 @@
 "use strict"
-
 /*
-Suskaiciuoti, kiek nurodytame intervale (0 -11; 8 - 31;) yra skaiciu, kurie dalijasi be liekanos is 3, 5 ir 7 atskirai
+Suskaiciuoti, kiek nurodytame intervale (0 -11; 8 - 31; -18 - 18) 
+yra skaiciu, kurie dalijasi be liekanos is 3, 5 ir 7 atskirai
 */
 
 const from = 0;
 const till = 11;
-const divider = 7;
+const divider = 5;
 
 let count = 0;
 
@@ -14,9 +14,10 @@ for (let i=from; i <= till; i++) {
     if (i % divider === 0);
         count++;
     }
-}
 
-const ats = `Skaiciu intervale tarp ${from} ir ${till}, besidalijanciu be liekanos is  ${divider} yra ${count} vienetai.`;
+
+const ats = `Skaiciu intervale tarp ${from} ir ${till}, besidalijanciu be liekanos is
+  ${divider} yra ${count} vienetai.`;
 
 console.log(ats);
 
